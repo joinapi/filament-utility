a3b2589e585d5ecb8c568bfa42822130b8bb1e03
JOIN API <devops@joinapi.io>
2025-01-03 23:17:39 -0300

    Cast

---
+++ src/Casts/DocumentCast.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Joinapi\FilamentUtility\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class DocumentCast  implements CastsAttributes
+{
+
+    public function get(Model $model, string $key, mixed $value, array $attributes)
+    {
+        return $value;
+    }
+
+    public function set(Model $model, string $key, mixed $value, array $attributes)
+    {
+        if (empty($value)) {
+            return null;
+        }
+        $newvalue = Str::of($value)->replaceMatches('/[^0-9]/', '');
+
+        if (empty($newvalue)) {
+            return null;
+        }
+
+        return $newvalue;
+    }
+}
